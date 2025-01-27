4dd07a54ce6dff5fe3b6f49d22de2bdd143424ba
abeeh <adambeehler@cmail.carleton.ca>
2025-01-27 13:08:59 -0500

    Base layout of files
    Signed-off-by: adam <adambeehler@cmail.carleton.ca>

---
+++ droneTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class droneTest {
+    @Test
+    void testEditThis() {
+        assertEquals(5, 5);
+    }
+}
---
+++ fireSubsystem.java
@@ -0,0 +1,8 @@
+/**
+ *  Fire Incident subsystem is to readin events using the format shown above: 
+ * Time, Zone ID, Event Type and Severity. Each line of input is to be sent to the Scheduler
+ */
+
+public class fireSubsystem {
+    
+}
