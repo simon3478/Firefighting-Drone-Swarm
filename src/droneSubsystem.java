9450f532632321ee590c8909790ac8a39ea81949
abeeh <adambeehler@cmail.carleton.ca>
2025-01-28 12:40:33 -0500

    Prepared base files for IntelliJ

---
+++ src/droneSubsystem.java
@@ -0,0 +1,8 @@
+/**
+ * Drones will make calls to the Scheduler which will then reply when there is work to
+ * be done. The Drone will then send the data back to the Scheduler who will then send
+ * it back to the Fire Incident subsystem
+ */
+
+public class droneSubsystem {
+}
---
+++ src/scheduler.java
@@ -0,0 +1,8 @@
+/**
+ * For Iteration 1, Scheduler is only being used as a communication channel from the Fire
+ * Incident Subsystem thread to the Drone thread and back again.
+ * This will be the server.
+ */
+
+public class scheduler {
+}
