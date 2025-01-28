40815e18e57d957d7abc772b53b42489f3971896
Adam <127047675+Adam-B6@users.noreply.github.com>
2025-01-28 12:24:38 -0500

    Delete fireSubsystem.java

--- fireSubsystem.java
+++
@@ -1,8 +0,0 @@
-/**
- *  Fire Incident subsystem is to readin events using the format shown above: 
- * Time, Zone ID, Event Type and Severity. Each line of input is to be sent to the Scheduler
- */
-
-public class fireSubsystem {
-    
-}
