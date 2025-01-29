603980ef6e9006db5a560e7d217244214ad0720e
Jared StLouis <jarstlouis@gmail.com>
2025-01-29 17:21:32 -0500

    input low, mod, high for enum class

--- src/FireIncident/SeverityLevel.java
+++ src/FireIncident/SeverityLevel.java
@@ -1,4 +1,13 @@
 package FireIncident;
 
+/**
+ * Enum representing the severity levels of a fire incident.
+ * 
+ * Low - Indicates a low-severity fire incident.
+ * Moderate - Indicates a moderate-severity fire incident.
+ * High - Indicates a high-severity fire incident.
+ * 
+ */
 public enum SeverityLevel {
+    Low, Moderate, High
 }
